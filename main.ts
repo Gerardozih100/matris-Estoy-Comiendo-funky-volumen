b12c388ba5f6ef10aa677ee02bd1dd756aaaff67
Gerardozih100 <106816077+Gerardozih100@users.noreply.github.com>
2022-10-15 10:52:00 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,46 @@
+input.onButtonPressed(Button.A, function () {
+    music.setVolume(255)
+})
+input.onButtonPressed(Button.B, function () {
+    music.setVolume(0)
+})
+max7219_matrix.setup(
+4,
+DigitalPin.P12,
+DigitalPin.P15,
+DigitalPin.P14,
+DigitalPin.P13
+)
+max7219_matrix.for_4_in_1_modules(
+rotation_direction.counterclockwise,
+true
+)
+let Musica = 0
+let Texto = 0
+basic.forever(function () {
+    if (Musica <= 10) {
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(233, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(196, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(196, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(349, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Half))
+        Musica += Musica + 1
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+    max7219_matrix.scrollText(
+    "Estoy Comiendo",
+    0,
+    500
+    )
+})
